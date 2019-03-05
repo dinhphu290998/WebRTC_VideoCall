1fcbdb8fb20a8e529e38400b4f1dd7441b5b7bd6
vMio <dohien11091999@gmail.com>
2019-03-05 17:06:05 +0700

    Update code

--- Pods/Target Support Files/Pods-Ai-Tec/Pods-Ai-Tec-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ai_Tec : NSObject
-@end
-@implementation PodsDummy_Pods_Ai_Tec
-@end
--- Pods/Target Support Files/Pods-Ai-TecTests/Pods-Ai-TecTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ai_TecTests : NSObject
-@end
-@implementation PodsDummy_Pods_Ai_TecTests
-@end
